4c9df706083405217cc1078b8fac4fe7712da0e5
Christine Contreras <christineec23@yahoo.com>
2021-04-05 16:49:23 -0500

    commit tree 36ea49c6304f73046a903edbc7c835b247f557cf

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31,
+      num2 = 2,
+      num3 = 14,
+      num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
